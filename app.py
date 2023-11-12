48c84b9b2a197c8a184ca000b6088ec9fbcbc5de
Pourya Hooshmand <pourya.hooshmand@aut.ac.ir>
2023-11-12 21:33:13 +0100

    [ADD] Simple route for search

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
 from flask import Flask
+from flask import request
+
+from whoosh.qparser import *
+from whoosh.index import open_dir
+from whoosh.fields import *
+from whoosh.analysis import STOP_WORDS
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def search_page():
     return "<p>Hello, World!</p>"
+
+@app.route("/search")
+def results_page():
+    search_text = request.args.get('q')
+    search_website = request.args.get('site')
+
+    ix = open_dir("indexdir")
+    with ix.searcher() as searcher:
+        parser = QueryParser("link", ix.schema)
+        parser.add_plugin(FuzzyTermPlugin())
+        #remove the ability to specify phrase queries inside double quotes.
+        parser.remove_plugin_class(PhrasePlugin)
+        #Adds the ability to group arbitrary queries inside double quotes,
+        #to produce a query matching the individual sub-queries in sequence.
+        parser.add_plugin(SequencePlugin())
+
+        search_words = search_text.split()
+        search_words = ["*" + _ + "*" for _ in search_words if _ not in STOP_WORDS]
+        if search_website:
+            search_query = u'link:*'+search_website+u'* content:(' + u' OR '.join(search_words) + u')'
+        else:
+            search_query = u'content:(' + u' OR '.join(search_words) + u')'
+        #return search_query
+        query = parser.parse(search_query)
+        results = searcher.search(query, limit = 20)
+        return "\n".join([str(r.values()) for r in results])
+
+        
